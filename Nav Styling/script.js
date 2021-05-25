4b8e406e63d7d35313353df78b2aa2a14dcccc29
shantanu-flutter <79631736+shantanu-flutter@users.noreply.github.com>
2021-05-25 10:01:35 +0530

    Nav Styling

---
+++ Nav Styling/script.js
@@ -0,0 +1,8 @@
+ const nav = document.getElementById('nav')
+ const icon = document.getElementById('btn')
+
+ icon.addEventListener('click', ()=>{
+     console.log('ye chala')
+     nav.classList.toggle('active')
+   
+ })
