974d4ca577592985bcf0b4f4ae671162b426562f
shantanu-flutter <79631736+shantanu-flutter@users.noreply.github.com>
2021-05-28 22:52:15 +0530

    Background Slider

---
+++ Background Slider/script.js
@@ -0,0 +1,35 @@
+const slides = document.querySelectorAll('.slide')
+const body  = document.body;
+const leftBtn = document.getElementById('left-arrow')
+const rightBtn = document.getElementById('right-arrow')
+
+let activeSlide = 0;
+setBackGround();
+
+function setBackGround() {
+    body.style.backgroundImage =
+     slides[activeSlide].style.backgroundImage;
+
+}
+
+leftBtn.addEventListener('click', ()=>{
+    slides[activeSlide].classList.remove('active')
+    if(activeSlide===0)activeSlide = slides.length-1;
+    else activeSlide--
+    slides[activeSlide].classList.add('active')
+    setBackGround();
+
+})
+
+
+
+rightBtn.addEventListener('click', ()=>{
+
+    console.log('changed ', activeSlide )
+    if(activeSlide===slides.length-1)activeSlide = 0;
+    else activeSlide++
+    slides[activeSlide].classList.add('active')
+
+    setBackGround();
+
+})
